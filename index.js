2b2a0a53a17e76338bb745d9e4f465462418bda2
Michael Duan <t-miduan@microsoft.com>
2018-07-09 11:41:03 -0700

    dfghj

--- index.js
+++ index.js
@@ -2,54 +2,61 @@ import './style';
 import { Component } from 'preact';
 
 export default class App extends Component {
+	startVideo(stream) {
+		const video = document.querySelector('#stream video');
+		video.src = window.URL.createObjectURL(stream);
+		video.play();
+	}
+
 	componentDidMount(){
-		var video = document.getElementById('video');
+		let video = document.getElementById('video');
+		//const canvas = document.querySelector('canvas');
+
+		//var mediaStreamTrack;
+		//var imageCapture;
+		var mediaStream;
+
+		function stopVideo(stream) {
+			const video = document.querySelector('video');
+			video.parentNode.removeChild(video);
+		
+			// free memory before page unloading
+			window.URL.revokeObjectURL(stream);
+		}
+		
+		function capture() {
+			// add canvas element
+			let canvas = document.createElement('canvas');
+		
+			// set canvas dimensions to video ones to not truncate picture
+			const videoElement = document.querySelector('video');
+			canvas.width = videoElement.width;
+			canvas.height = videoElement.height;
+		
+			// copy full video frame into the canvas
+			document.getElementsByClassName('thumbnailContainer')[0].appendChild(canvas);
+			canvas.getContext('2d').drawImage(videoElement, 0, 0, videoElement.width, videoElement.height);
+		
+			// get image data URL and remove canvas
+			const snapshot = canvas.toDataURL('image/png');
+			canvas.parentNode.removeChild(canvas);
+		
+			// update grid picture source
+			document.querySelector('#grid').setAttribute('src', snapshot);
+		}
+
 		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-			 navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-			 video.src = window.URL.createObjectURL(stream);
-			 video.play();
+			navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+				mediaStream = stream;
+				video.src = window.URL.createObjectURL(stream);
+				video.play();
 			});
 		}
 
-		let i = 0;
-
-		video.addEventListener('loadeddata', function() {
-			this.currentTime = i;
+		document.getElementById('capture').addEventListener('click', () => {
+			capture();
+			stopVideo(mediaStream);
 		});
-
-		video.addEventListener('seeked', function() {
-
-			// now video has seeked and current frames will show
-			// at the time as we expect
-			generateThumbnail(i);
-		
-			// when frame is captured increase, here by 5 seconds
-			i += 5;
-		
-			// if we are not passed end, seek to next interval
-			if (i <= this.duration) {
-				// this will trigger another seeked event
-				this.currentTime = i;
-			}
-			else {
-				// Done!, next action
-			}
-		});
-
-		function generateThumbnail(i) {
-			//generate thumbnail URL data
-			let thecanvas = document.getElementByClass('canvas');
-			let context = thecanvas.getContext('2d');
-			context.drawImage(video, 0, 0, 220, 150);
-			let dataURL = thecanvas.toDataURL();
-		
-			//create img
-			let img = document.createElement('img');
-			img.setAttribute('src', dataURL);
-		
-			//append img in container div
-			document.getElementByClass('canvas').appendChild(img);
-		}
 	}
 
 
@@ -58,10 +65,16 @@ export default class App extends Component {
 			<div class="thumbnailContainer">
 				<center>
 					<video id="video" width="640" height="480" autoplay />
-					<canvas class="canvas" />
 				</center>
+				<div id="stream"></div>
+				<img id="grid" src="#" />
+				<p class="actions"><button id="capture">Capture</button></p>
 			</div>
 		);
 
 	}
+
+	/* <center>
+					<video id="video" width="640" height="480" autoplay />
+				</center> */
 }
